925ae1e8303278ac93543af0eacff2bc2d2d1a67
MZRaymundo <mz.raym@gmail.com>
2017-12-05 03:44:42 +0800

    Update Exercise2.rb

--- Exercise2.rb
+++ Exercise2.rb
@@ -8,8 +8,6 @@
 
     |--Variables used ------------------------------------------------------------------------------|
     |  input               |  the number of print statements the user wants to print                |
-    |  counter             |  a counter for the number of times the procedure of copy pasting done  |
-    |  totalStatements     |  the total number of statements that can possibly be copy pasted       |
     |  result              |  the output - the number of times the procedure of copy pasting done   |
     |-----------------------------------------------------------------------------------------------|
 =end
@@ -32,26 +30,6 @@ def printResult(result)
   puts "======================== END ========================\n\n"
 end
 
-#computes for the least number of times the print statement can be copy pasted.
-def computeNumOfTimesCopied(input)
-  counter = 0
-  totalStatements = 1
-
-  # continue to do the procedure when totalStatements is less than what is required by the user.
-  while totalStatements < input
-    if(totalStatements*2 <= input)                    # START OF IF ELSE STATEMENT ----------------------------------------------------------------------------
-      totalStatements *= 2                            # this checks if the results of the totalStatements when copied at max is more than the input
-    else                                              # if it isn't, proceed to copying at max, else, only copy the number of lines needed to complete the code
-      totalStatements += (input - totalStatements)    # this part of the code prevents the user to delete lines after pasting.
-    end                                               # END OF IF ELSE STATEMENT ------------------------------------------------------------------------------
-
-    counter +=1                                       # counter increments as the procedure has been done
-  end
-
-  return counter;
-end
-
 #main program
 input = getInput
-result = computeNumOfTimesCopied(input)
-printResult(result)
+printResult(Math.log2(input).ceil)
